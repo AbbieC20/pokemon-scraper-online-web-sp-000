9bbcb23e9dab71ff170086bcf328c055f4e94bfc
remote-torrent-6747 <abbyc@sky.com>
2021-03-19 20:07:06 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -13,7 +13,7 @@ class Pokemon
         INSERT INTO pokemon (name, type)
         VALUES (?, ?)
         SQL
-    db.execute(sql, self.name, self.type)
+    db.execute(sql, name, type)
     id = db.execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
     Pokemon.new(id: id, name: name, type: type, db: db)
   end
