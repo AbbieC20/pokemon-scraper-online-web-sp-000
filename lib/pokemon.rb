2fb006b8b68ec013cd6dd96abc0c99613e16f4df
remote-torrent-6747 <abbyc@sky.com>
2021-03-19 19:55:09 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,20 @@
 class Pokemon
+  attr_accessor :id, :name, :type, :db
+
+  def initialize (id:, name:, type:, db:)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+  end
+
+  def self.save(name, type, db)
+    sql = <<-SQL
+        INSERT INTO pokemon (name, type)
+        VALUES (?, ?)
+        SQL
+    @db[:conn].execute(sql)
+    @id = @db[:conn].execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
+  end
+
 end
