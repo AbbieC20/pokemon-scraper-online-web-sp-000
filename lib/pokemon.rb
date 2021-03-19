aa9756cd4b5531c7cd33abeac95842b2d6e33e79
remote-torrent-6747 <abbyc@sky.com>
2021-03-19 20:02:56 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -15,7 +15,7 @@ class Pokemon
         SQL
     db.execute(sql)
     id = db.execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
-    Pokemon.new(id: id, name: name, type: type, db: dn)
+    Pokemon.new(id: id, name: name, type: type, db: db)
   end
 
 end
